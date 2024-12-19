49244d5e5f3d36c8a8e51eef97431799334dd6df
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-12-18 23:57:10 -0500

    CI stuff

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'test']
+        ],
+    }
+};
