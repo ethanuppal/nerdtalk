f66c02ee2c4b69465414a4237acfde2f037ed4dc
feverdreme <jeffreyhuang08@gmail.com>
2024-12-20 03:17:55 -0500

    Configure tailwind and add basic components
    Signed-off-by: Jeffrey Huang <jeffreyhuang08@gmail.com>

---
+++ app/src/components/TextBox.tsx
@@ -0,0 +1,16 @@
+interface TextBoxParams {
+    messageBody: string;
+    timestamp: string;
+    slotnum?: number;
+}
+
+export default function TextBox(props: TextBoxParams) {
+    const {messageBody, timestamp} = props;
+
+    return <div className="bg-primary-color rounded border border-blue-400 px-4 py-2">
+        <p>{messageBody} {timestamp}</p>
+
+        {/* Bro needs to float top right */}
+        {/* <p>{timestamp}</p> */}
+    </div>
+};
