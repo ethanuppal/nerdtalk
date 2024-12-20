181b481baa10ce6f752cf81097635c04f64a26eb
feverdreme <jeffreyhuang08@gmail.com>
2024-12-20 03:17:55 -0500

    Configure tailwind and add basic components

---
+++ app/src/components/MessageLog.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import TextBox from "@components/TextBox";
+
+interface Message {
+  body: string;
+  timestamp: string;
+  sender: string;
+  slotnum?: number;
+}
+
+export default function MessageLog() {
+  const [log, setLog] = useState<Message[]>([]);
+
+  // FIXME: Just for testing purposes
+  useEffect(() => {
+    const msg1: Message = { body: "test1", timestamp: "hi", sender: "Haadi" };
+    const msg2: Message = { body: "test2", timestamp: "hi2", sender: "Ethan" };
+    setLog([msg1, msg2]);
+  }, []);
+
+  return (
+    <div>
+      {log.map((message) => (
+        <TextBox timestamp={message.timestamp} messageBody={message.body} />
+      ))}
+    </div>
+  );
+}
