372398851da5000c363ec5908cb561f9653a7cb1
feverdreme <jeffreyhuang08@gmail.com>
2024-12-20 22:51:37 -0500

    Add prettier and reformat code
    Signed-off-by: Jeffrey Huang <jeffreyhuang08@gmail.com>

--- app/src/components/TextBox.tsx
+++ app/src/components/TextBox.tsx
@@ -1,16 +1,20 @@
 interface TextBoxParams {
-    messageBody: string;
-    timestamp: string;
-    slotnum?: number;
+  messageBody: string
+  timestamp: string
+  slotnum?: number
 }
 
 export default function TextBox(props: TextBoxParams) {
-    const {messageBody, timestamp} = props;
+  const { messageBody, timestamp } = props
 
-    return <div className="bg-primary-color rounded border-none px-2 py-2">
-        <p>{messageBody} {timestamp}</p>
+  return (
+    <div className="w-fit rounded border-none bg-primary-color px-2 py-2">
+      <p>
+        {messageBody} {timestamp}
+      </p>
 
-        {/* Bro needs to float top right */}
-        {/* <p>{timestamp}</p> */}
+      {/* Bro needs to float top right */}
+      {/* <p>{timestamp}</p> */}
     </div>
-};
+  )
+}
