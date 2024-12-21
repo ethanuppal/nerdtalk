74b60e3914de5878eb6bf192982abd8deb3beb60
feverdreme <jeffreyhuang08@gmail.com>
2024-12-20 22:53:47 -0500

    Prettier format all files

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,17 @@
 // import "./App.css";
 
-import InputBox from "@components/InputBox";
-import MessageLogDisplay from "@components/MessageLogDisplay";
-import messageLogStore from "@state/messageLogStore";
+import InputBox from '@components/InputBox'
+import MessageLogDisplay from '@components/MessageLogDisplay'
+import messageLogStore from '@state/messageLogStore'
 
 function App() {
   return (
-    <main className="h-screen flex flex-col gap-2 p-2 border-none">
+    <main className="flex h-screen flex-col gap-2 border-none p-2">
       <MessageLogDisplay messageLogStore={messageLogStore} />
 
       <InputBox messageLogStore={messageLogStore} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
