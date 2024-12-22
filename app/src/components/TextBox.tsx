8608208a38584759813b0839ca350b59a4befec4
feverdreme <jeffreyhuang08@gmail.com>
2024-12-22 03:38:50 -0500

    Complete discord new author text styling
    Signed-off-by: Jeffrey Huang <jeffreyhuang08@gmail.com>

--- app/src/components/TextBox.tsx
+++ app/src/components/TextBox.tsx
@@ -13,8 +13,28 @@ export interface TextBoxProps {
 export default function TextBox(props: TextBoxProps) {
   const { type, body, timestamp, author } = props
 
-  return <div className="className=py-2 text-white hover:bg-gray-600">
-    <p><span className="font-bold">{author}</span> <time className="text-gray-400 text-xs">{timestamp}</time> </p>
-    <p>{body}</p>
-  </div>
+  if (type == TextBoxType.Authored) return (
+    <div className="flex gap-2 p-2 text-white hover:bg-gray-600">
+      <img src="/image.png" className={"w-10 rounded-full" + (author == "ethan" ? " invert" : "")} />
+
+      <div>
+        <p>
+          <span className="font-bold">{author}</span>{' '}
+          <time className="text-xs text-gray-400">{timestamp}</time>{' '}
+        </p>
+        <p>{body}</p>
+      </div>
+    </div>
+  ) 
+  else return (
+    <div className="flex gap-2 px-2 hover:bg-gray-600 text-secondary-color hover:text-gray-400">
+      <div className="grid h-5 w-10 place-content-center">
+        <div className="h-4 text-center">
+          <time className="box-content block text-xs">{timestamp}</time>
+        </div>
+      </div>
+      <p className="!text-white">{body}</p>
+    </div>
+  ) 
+
 }
