bb798fd7e0e5862de972cba958a4cb5fd13f81bc
feverdreme <jeffreyhuang08@gmail.com>
2024-12-23 01:21:09 -0500

    Implement multiple authors

--- app/src/state/authorStore.ts
+++ app/src/state/authorStore.ts
@@ -1,22 +1,40 @@
-import { makeObservable, observable, action } from 'mobx'
+import { makeObservable, action, observable } from 'mobx'
 
 export interface Author {
     name: string;
     avatarURL: string;
 }
 
+export type AuthorRef = number
+
 export class AuthorStore {
-    authors: Set<Author> = new Set()
+    // TODO: Enforce uniqueness through keys
+    authors: Author[] = []
 
     constructor() {
         makeObservable(this, {
             authors: observable,
-            addAuthor: action
+            addAuthor: action,
+        })
+
+        this.addAuthor({
+          name: 'Haadi',
+          avatarURL:
+            'https://cdn.discordapp.com/avatars/295595875168813058/9480345ded6da896d57957539bd4f881.webp?size=80',
+        })        
+        this.addAuthor({
+          name: 'Haadi2',
+          avatarURL:
+            'https://cdn.discordapp.com/avatars/295595875168813058/9480345ded6da896d57957539bd4f881.webp?size=80',
         })
     }
 
     addAuthor(author: Author) {
-        this.authors.add(author)
+        this.authors.push(author)
+    }
+
+    getAuthor(authorRef: AuthorRef): Author {
+        return this.authors[authorRef];
     }
 }
 
