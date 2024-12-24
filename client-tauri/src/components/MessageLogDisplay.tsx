0e751c95dc5f0f1230b80e7d0dd615440c386d8c
Jeffrey Huang <jeffreyhuang08@gmail.com>
2024-12-24 16:30:52 -0500

    Rename "Authors" to "Users"

--- client-tauri/src/components/MessageLogDisplay.tsx
+++ client-tauri/src/components/MessageLogDisplay.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TextBox from '@components/TextBox'
 import { observer } from 'mobx-react-lite'
 import { MessageLogStore } from '@state/messageLogStore'
-import authorStore from '@state/authorStore'
+import userStore from '@state/authorStore'
 
 interface MessageLogDisplayProps {
   messageLogStore: MessageLogStore
@@ -13,7 +13,7 @@ const MessageLogDisplay: React.FC<MessageLogDisplayProps> = observer(
     return (
       <div className="flex flex-grow flex-col gap-1 overflow-scroll rounded border-none bg-primary-color py-2 text-sm">
         {messageLogStore.foldAuthors.map((message) => (
-          <TextBox authorStore={authorStore} {...message} />
+          <TextBox userStore={userStore} {...message} />
         ))}
       </div>
     )
