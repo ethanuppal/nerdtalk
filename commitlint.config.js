b8a5198dfad7f5af856627b9dd7641d29a2092bf
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-12-29 20:59:28 -0700

    ci(lint): Better PR title linting

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'test']
-        ],
-    }
-};
