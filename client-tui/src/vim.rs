46c4d8711bbe57c725a4bdbf81dd92d6eb10db95
Haadi Khan <haadikhan05@gmail.com>
2025-01-01 15:13:53 -0500

    Remove unused var

--- client-tui/src/vim.rs
+++ client-tui/src/vim.rs
@@ -313,7 +313,6 @@ impl EditingContext {
         &mut self,
         text: &mut String,
         clipboard: &mut ClipboardContext,
-        height: u16,
         command: Command,
     ) {
         match command {
